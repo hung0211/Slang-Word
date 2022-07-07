1838a36d24a7c86cff305e42326c4a235fda1fed
hung0211 <nguyenphihung.tech@gmail.com>
2022-07-07 17:08:34 +0700

    Quiz by definition

--- Slang_Word/src/slang_word/main.java
+++ Slang_Word/src/slang_word/main.java
@@ -59,6 +59,9 @@ public class main {
                 case "9":
                     quizByKey();
                     break;
+                case "10":
+                    quizByDefinition();
+                    break;
                 case "0":
                     System.out.println("exited!");
                     exit = true;
@@ -287,6 +290,33 @@ public class main {
         System.out.println("2. " + answer.get(1).definition);
         System.out.println("3. " + answer.get(2).definition);
         System.out.println("4. " + answer.get(3).definition);
+        System.out.print("\nYour choose is: ");
+        int choose = kb.nextInt();
+        if (choose - 1 == correct) {
+            System.out.println("Congrat!!! Correct answer !!!");
+        } else {
+            System.out.println("Oops!!! Wrong answer");
+        }
+    }
+    
+    public static void quizByDefinition() {
+        Random rd = new Random();
+        List<Entry> answer = new ArrayList<Entry>();
+        Entry temp = randomSlangWord();
+        for (int i = 0; i < 4; i++) {
+            while (answer.contains(temp)) {
+                temp = randomSlangWord();
+            }
+            answer.add(temp);
+        }
+        int correct = rd.nextInt(3);
+        Scanner kb = new Scanner(System.in);
+        System.out.println("\n^^ The mysterious slang word is: " + answer.get(correct).definition);
+        System.out.println("Answer: ");
+        System.out.println("1. " + answer.get(0).key);
+        System.out.println("2. " + answer.get(1).key);
+        System.out.println("3. " + answer.get(2).key);
+        System.out.println("4. " + answer.get(3).key);
         System.out.print("\nYour choose is: ");
         int choose = kb.nextInt();
         if (choose - 1 == correct) {
